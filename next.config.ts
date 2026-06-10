3878afe22807e5acdb75337b28e92aeeecd393de
firsovaellenn <firsovaellenn@gmail.com>
2026-06-10 20:17:30 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/elenafirsova",
   images: {
     unoptimized: true,
   },
-
   allowedDevOrigins: ["*"],
-
   poweredByHeader: false,
   reactStrictMode: true,
 };
