88478c77af0b8ac6874cd99da4a2e5ee8fd83a40
firsovaellenn <firsovaellenn@gmail.com>
2026-06-11 19:42:38 +0300

    Update portfolio-photos.ts

--- lib/portfolio-photos.ts
+++ lib/portfolio-photos.ts
@@ -5,6 +5,12 @@ export interface RepoPhoto {
 }
 
 export const repoPhotos: RepoPhoto[] = [
-  // Пример:
-  // { id: "photo-1", src: "/elenafirsova/photos/photo-1.jpg", alt: "Фото 1" },
+  { id: "photo-1", src: "/elenafirsova/photos/2023-06-12 19.58.24.JPG", alt: "Фото 1" },
+  { id: "photo-2", src: "/elenafirsova/photos/2023-06-18 19.54.57.PNG", alt: "Фото 2" },
+  { id: "photo-3", src: "/elenafirsova/photos/2023-06-19 21.02.16.JPG", alt: "Фото 3" },
+  { id: "photo-4", src: "/elenafirsova/photos/2023-06-23 16.21.27.PNG", alt: "Фото 4" },
+  { id: "photo-5", src: "/elenafirsova/photos/2023-06-23 16.22.56.JPG", alt: "Фото 5" },
+  { id: "photo-6", src: "/elenafirsova/photos/2023-06-23 16.26.46.JPG", alt: "Фото 6" },
+  { id: "photo-7", src: "/elenafirsova/photos/2023-06-23 17.32.05.JPG", alt: "Фото 7" },
+  { id: "photo-8", src: "/elenafirsova/photos/2023-07-04 11.10.13.JPG", alt: "Фото 8" },
 ];
